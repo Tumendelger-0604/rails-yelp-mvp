916323770e934c034ba8dcac1d40aaa7ed2c710d
Tumendelger Gankhuyag <tumee666@gmail.com>
2023-09-08 17:06:52 +0200

    rails-yelp

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,72 +1,29 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: [:show]
-
   def index
     @restaurants = Restaurant.all
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
     @review = Review.new
   end
 
-  # Other controller actions (new, create, edit, update, destroy)...
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
 
   private
 
-  def set_restaurant
-    @restaurant = Restaurant.find(params[:id])
-  end
-end
-
-describe "GET show" do
-  it "routes to #show" do
-    expect(get: "/restaurants/1").to route_to("restaurants#show", id: "1")
-  end
-
-
-end
-
-describe "GET new" do
-  it "routes to #new" do
-    expect(get: "/restaurants/new").to route_to("restaurants#new")
-  end
-
-
-end
-
-def show
-  @restaurant = Restaurant.find(params[:id])
-
-end
-
-def new
-  @restaurant = Restaurant.new
-
-end
-
-describe "POST create" do
-  context "with valid params" do
-    it "creates a new Restaurant" do
-      expect {
-        post :create, params: { restaurant: valid_attributes }
-      }.to change(Restaurant, :count).by(1)
-    end
-  end
-
-  context "with invalid params" do
-    it "assigns a newly created but unsaved restaurant as @restaurant" do
-      post :create, params: { restaurant: invalid_attributes }
-      expect(assigns(:restaurant)).to be_a_new(Restaurant)
-    end
-  end
-end
-
-describe "POST create" do
-  context "with invalid params" do
-    it "assigns a newly created but unsaved review as @review" do
-      post :create, params: { restaurant_id: restaurant.id, review: invalid_attributes }
-      expect(assigns(:review)).to be_a_new(Review)
-    end
-
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_16_151029) do
+ActiveRecord::Schema[7.0].define(version: 2022_03_17_114651) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
@@ -21,8 +21,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_16_151029) do
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.integer "rating"
     t.text "content"
+    t.integer "rating"
     t.integer "restaurant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
